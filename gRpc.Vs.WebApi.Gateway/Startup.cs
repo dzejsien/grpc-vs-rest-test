f022c4fe06cc175ba8210588957f1c3d569ac58c
Sebastian Bojarczak <sboja@softserveinc.com>
2019-12-06 15:34:43 +0100

    dockerfiles for gateway, rest-server and grcp-server + cert

--- gRpc.Vs.WebApi.Gateway/Startup.cs
+++ gRpc.Vs.WebApi.Gateway/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.EventLog;
 
 namespace gRpc.Vs.WebApi.Gateway
 {
@@ -35,48 +34,52 @@ namespace gRpc.Vs.WebApi.Gateway
         {
             Configuration.GetSection("Urls").Bind(_urls);
 
+            Func<HttpClientHandler> configurePrimaryHttpMessageHandler = () =>
+            {
+                var handler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (request, cert, chain, errors) =>
+                    {
+                        Console.WriteLine($"SslPolicyErrors: {errors}");
+
+                        if (chain == null)
+                        {
+                            Console.WriteLine("No chain...");
+                        }
+                        else
+                        {
+                            foreach (X509ChainElement element in chain.ChainElements)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine(element.Certificate.Subject);
+
+                                foreach (X509ChainStatus status in element.ChainElementStatus)
+                                {
+                                    Console.WriteLine($"  {status.Status}: {status.StatusInformation}");
+                                }
+                            }
+                        }
+
+                        return true;
+                    }
+                };
+
+                return handler;
+            };
+
             services.AddControllers();
             services.AddGrpcClient<GrpcDataService.GrpcDataServiceClient>(c =>
             {
                 c.Address = new Uri(_urls.GrpcServer);
             });
+            //.ConfigurePrimaryHttpMessageHandler(configurePrimaryHttpMessageHandler);
+
             services.AddHttpClient<IDataClient, DataClient>(c =>
             {
                 c.BaseAddress = new Uri(_urls.RestServer);
             });
-            // for diagnostics of cert - which was used etc
-            //.ConfigurePrimaryHttpMessageHandler(_ =>
-            //    {
-            //        var handler = new HttpClientHandler
-            //        {
-            //            ServerCertificateCustomValidationCallback = (request, cert, chain, errors) =>
-            //            {
-            //                Console.WriteLine($"SslPolicyErrors: {errors}");
-
-            //                if (chain == null)
-            //                {
-            //                    Console.WriteLine("No chain...");
-            //                }
-            //                else
-            //                {
-            //                    foreach (X509ChainElement element in chain.ChainElements)
-            //                    {
-            //                        Console.WriteLine();
-            //                        Console.WriteLine(element.Certificate.Subject);
-
-            //                        foreach (X509ChainStatus status in element.ChainElementStatus)
-            //                        {
-            //                            Console.WriteLine($"  {status.Status}: {status.StatusInformation}");
-            //                        }
-            //                    }
-            //                }
-
-            //                return true;
-            //            }
-            //        };
-
-            //        return handler;
-            //    });
+            // for diagnostics of cert - which was used etc 
+            //.ConfigurePrimaryHttpMessageHandler(configurePrimaryHttpMessageHandler);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
